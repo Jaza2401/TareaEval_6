033073cd26a26535d50dea577ea10f636dacab9b
Jaza2401 <alex.jauregui.zarate@gmail.com>
2023-03-09 19:29:08 -0700

    Final 09/03

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
+  items: any[] = [];
+  ngOnInit() {
+    const data = localStorage.getItem('data'); 
+    console.log(data);
 
+    if (data) {
+      const parsed_data = JSON.parse(data);   
+      console.log(parsed_data);
+      this.items = [];
+      for (let i = 0; i < parsed_data["names"].length; i++) {
+        this.items.push({
+          id: parsed_data["ids"][i],
+          name: parsed_data["names"][i],
+          price: parsed_data["prices"][i],
+        });
+      }
+    }
+}
 }

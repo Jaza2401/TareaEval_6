e9defc036bd3e13db7f02b44d363c4fd971a9622
m <m@tag.local>
2023-03-09 18:36:34 -0700

    Agregue funcionalidad a agregar productos

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
+
 export class CreateComponent {
 
+    newItemID = '';
+    newItemName = '';
+    newItemPrice = '';
+
+    addItem() {
+      
+      const data = localStorage.getItem('data'); 
+      let json;
+
+      if (data) {
+         const parsed_data = JSON.parse(data);
+         json  = {
+          ids: parsed_data["ids"].concat([this.newItemID]), 
+          names: parsed_data["names"].concat([this.newItemName]), 
+          prices: parsed_data["prices"].concat([this.newItemPrice])
+        };
+      }
+      else {
+        json = {
+          ids: [this.newItemID], 
+          names: [this.newItemName], 
+          prices: [this.newItemPrice], 
+        };
+      }
+      localStorage.setItem("data", JSON.stringify(json));
+      window.location.reload();
+
+  }
 }
